2d9e01c353940a2c0ec81c3b0129ad00654d6759
shmulikmak <shmulikmak@gmail.com>
2019-07-01 18:16:27 +0300

    before add DTO

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,17 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './tasks.model';
+import { CreateClassDto } from './dto/create-task.dto';
 
 @Controller('tasks')
 export class TasksController {
-    constructor(private tasksService: TasksService) {}
+    constructor(private tasksService: TasksService) { }
 
     @Get()
     getAllTasks(): Task[] {
         return this.tasksService.getAllTasks();
     }
 
-    createTask(title: string, description: string) {
-
+    @Post()
+    createTask(
+        @Body('title') title: string,
+        @Body('description') description: string,
+    ): Task {
+       return this.tasksService.createTask(title, description);
     }
 }
